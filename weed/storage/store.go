fefc3bb29045b41833f172936e19b71496f3e8d3
coffeecloudgit <168399738+coffeecloudgit@users.noreply.github.com>
2024-12-16 10:06:37 +0800

    Update store.go
    Add lock for location.ecVolumes when for each location.ecVolumes

--- weed/storage/store.go
+++ weed/storage/store.go
@@ -123,7 +123,7 @@ func (s *Store) DeleteCollection(collection string) (e error) {
 	for _, location := range s.Locations {
 		e = location.DeleteCollectionFromDiskLocation(collection)
 		if e != nil {
-			return
+			returndeleteExpiredEcVolumes
 		}
 		stats.DeleteCollectionMetrics(collection)
 		// let the heartbeat send the list of volumes, instead of sending the deleted volume ids to DeletedVolumesChan
@@ -381,6 +381,7 @@ func (s *Store) CollectHeartbeat() *master_pb.Heartbeat {
 
 func (s *Store) deleteExpiredEcVolumes() (ecShards, deleted []*master_pb.VolumeEcShardInformationMessage) {
 	for _, location := range s.Locations {
+		location.ecVolumesLock.RLock()
 		for _, ev := range location.ecVolumes {
 			messages := ev.ToVolumeEcShardInformationMessage()
 			if ev.IsTimeToDestroy() {
@@ -395,6 +396,7 @@ func (s *Store) deleteExpiredEcVolumes() (ecShards, deleted []*master_pb.VolumeE
 				ecShards = append(ecShards, messages...)
 			}
 		}
+		location.ecVolumesLock.RUnlock()
 	}
 	return
 }
